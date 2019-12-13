1829927c85a53a39c97a5ac4f78a4206725f05db
dellingc <35514357+dellingc@users.noreply.github.com>
2019-12-13 12:28:00 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+function showImg() {
+    document.getElementById('jesus').classList.remove("hidden")
+    document.getElementById('jesus').classList.add("visible")
+}
+
+function showImg2() {
+    setTimeout(function(){
+       document.getElementById('jesus2').classList.remove("hidden")
+    document.getElementById('jesus2').classList.add('visible') 
+    }, 5000)
+}
+
+function showImg3() {
+    setTimeout(function(){
+       document.getElementById('jesus3').classList.remove("hidden")
+    document.getElementById('jesus3').classList.add('visible') 
+    }, 8000)
+}
+
+function showImg4() {
+    setTimeout(function(){
+       document.getElementById('jesus4').classList.remove("hidden")
+    document.getElementById('jesus4').classList.add('visible') 
+    }, 11000)
+}
+
+function showImg5() {
+    setTimeout(function(){
+       document.getElementById('jesus5').classList.remove("hidden")
+    document.getElementById('jesus5').classList.add('visible') 
+    }, 14000)
+}
+
+function showImg6() {
+    setTimeout(function(){
+       document.getElementById('jesus6').classList.remove("hidden")
+    document.getElementById('jesus6').classList.add('visible') 
+    }, 17000)
+}
+
+function showImg7() {
+    setTimeout(function(){
+       document.getElementById('jesus7').classList.remove("hidden")
+    document.getElementById('jesus7').classList.add('visible') 
+    }, 20000)
+}
+
+function showImg8() {
+    setTimeout(function(){
+       document.getElementById('jesus8').classList.remove("hidden")
+    document.getElementById('jesus8').classList.add('visible') 
+    }, 23000)
+}
+
+function showImg9() {
+    setTimeout(function(){
+       document.getElementById('jesus9').classList.remove("hidden")
+    document.getElementById('jesus9').classList.add('visible') 
+    }, 26000)
+}
+
+function showImg10() {
+    setTimeout(function(){
+       document.getElementById('jesus10').classList.remove("hidden")
+    document.getElementById('jesus10').classList.add('visible') 
+    }, 29000)
+}
+
+function showImg11() {
+    setTimeout(function(){
+       document.getElementById('jesus11').classList.remove("hidden")
+    document.getElementById('jesus11').classList.add('visible') 
+    }, 32000)
+}
